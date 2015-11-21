56a43f3ee453787c4597636de994b5b06fe46ab3
alecor <alecor.dev@gmail.com>
2015-11-21 16:42:57 +0000

    Documentation link, examples added, exception handling improvement

--- argparse_simplest.py
+++ argparse_simplest.py
@@ -1,3 +1,7 @@
+"""
+https://docs.python.org/2/library/argparse.html
+"""
+
 import sys
 import argparse
 
@@ -13,7 +17,11 @@ if __name__ == '__main__':
         parser.add_argument('-l', '--log', metavar=('SAMPLE1'), help='1 var expected: SAMPLE1')
         parser.add_argument('-t', '--two', nargs=2, metavar=('VAR1', 'VAR2'), help='2 vars expected: VAR1 VAR2')
         parser.add_argument('-f', '--flag', action='store_true', help='Flag option (True/False)')
-        parser.add_argument('-d', '--default', help='default behaviour var')
+        parser.add_argument('-d', '--default', help='help text for default')
+
+        parser.add_argument('var', type=int, help='var will be considered int')
+        parser.add_argument('-v', '--verbosity', type=int, choices=[0, 1, 2], help='increase output verbosity')
+        parser.add_argument('-c', '--count', action='count', help='count help', default=0)
         args = parser.parse_args()
 
         if len(sys.argv) < 2:
@@ -27,5 +35,5 @@ if __name__ == '__main__':
             print(args.flag)
             main()
 
-    except Exception, e:
+    except Exception as e:
         print(e)
